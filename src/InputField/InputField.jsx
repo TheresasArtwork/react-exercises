79fc3f7c9a8540432bc994193da0e851231fdb04
Theresa Husske <theresa.husske@t-online.de>
2023-11-27 20:20:47 +0100

    added a conditional operator to change text

--- src/InputField/InputField.jsx
+++ src/InputField/InputField.jsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 export function InputField() {
   const [value, setValue] = useState(0);
   let multiplier = 2;
-  let sum = value * multiplier;
+  let result = value * multiplier;
 
   return (
     <div className="input-field-container">
       <input type="number" onChange={(e) => setValue(e.target.value)} />
       <p>
-        You entered {value}. Multiplied by {multiplier}, that equals {sum}
+        {value
+          ? `You entered ${value}. Multiplied by ${multiplier}, that equals ${result}.`
+          : `Please enter a value.`}
       </p>
     </div>
   );
