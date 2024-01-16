cffc84cb11e29155a81c5b38eabcea69395cf5b8
Theresa Husske <theresa.husske@t-online.de>
2024-01-16 12:02:38 +0100

    inplemented buttons from mui (vacation)

--- src/Vacation/Vacation.jsx
+++ src/Vacation/Vacation.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import beachImage from "../assets/beach1280.jpg";
 import winterImage from "../assets/winter-landscape1280.jpg";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import "./Vacation.css";
 
 export function Vacation() {
@@ -9,7 +11,35 @@ export function Vacation() {
   return (
     <div className="vacation-container">
       <h3>Choose your dream vacation!</h3>
-      <div className="button-container">
+      <ButtonGroup
+        size="large"
+        variant="outlined"
+        aria-label="outlined primary button group"
+      >
+        <Button
+          className={image === winterImage ? "button-active" : ""}
+          onClick={() => setImage(winterImage)}
+        >
+          Winter Wonderland
+        </Button>
+        <Button
+          className={image === beachImage ? "button-active" : ""}
+          onClick={() => setImage(beachImage)}
+        >
+          Tropical Island
+        </Button>
+      </ButtonGroup>
+      <div className="image-container">
+        <img src={image}></img>
+      </div>
+    </div>
+  );
+}
+
+/*
+OHNE MUI 
+
+<div className="button-container">
         <button
           className={image === winterImage ? "button-active" : ""}
           onClick={() => setImage(winterImage)}
@@ -22,10 +52,4 @@ export function Vacation() {
         >
           Tropical Island
         </button>
-      </div>
-      <div className="image-container">
-        <img src={image}></img>
-      </div>
-    </div>
-  );
-}
+      </div>*/
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { indigo } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: indigo[50],
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
